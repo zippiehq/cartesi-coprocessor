a21156047a7cfb317e191310c944f9da305c2931
Yelyzaveta Dymchenko <yelyzaveta.dymchenko@zippie.com>
2024-12-05 09:44:43 +0000

    feat: implement operator registering in anvil

--- setup-operator/src/main.rs
+++ setup-operator/src/main.rs
@@ -1,7 +1,6 @@
 use alloy_primitives::{Address, Bytes, FixedBytes, U256};
 use alloy_signer_local::PrivateKeySigner;
 use clap::Parser;
-use setup_operator::Options;
 use eigen_client_avsregistry::writer::AvsRegistryChainWriter;
 use eigen_client_elcontracts::{
     reader::ELChainReader,
@@ -9,28 +8,54 @@ use eigen_client_elcontracts::{
 };
 use eigen_crypto_bls::BlsKeyPair;
 use eigen_logging::get_test_logger;
-use eigen_testing_utils::{
-    anvil_constants::{
-        get_avs_directory_address, get_delegation_manager_address,
-        get_operator_state_retriever_address, get_registry_coordinator_address,
-        get_rewards_coordinator_address, get_strategy_manager_address,
-    },
-    transaction::wait_transaction,
-};
+use eigen_testing_utils::transaction::wait_transaction;
+use eigen_utils::erc20::ERC20;
+use eigen_utils::get_signer;
+use eigen_utils::strategymanager::StrategyManager;
+use setup_operator::Options;
 use std::{
+    fs::File,
+    path::Path,
     str::FromStr,
     time::{SystemTime, UNIX_EPOCH},
 };
+
 #[tokio::main]
 async fn main() {
     let opt = Options::parse();
     let signer = PrivateKeySigner::from_str(&opt.operator_private_key).unwrap();
-    let delegation_manager_address =
-        get_delegation_manager_address(opt.http_endpoint.to_owned()).await;
-    let avs_directory_address = get_avs_directory_address(opt.http_endpoint.to_owned()).await;
-    let strategy_manager_address = get_strategy_manager_address(opt.http_endpoint.to_owned()).await;
-    let rewards_coordinator_address =
-        get_rewards_coordinator_address(opt.http_endpoint.to_owned()).await;
+
+    let deployment_parameters_devnet = File::open(Path::new(
+        &opt.chain_writer_reader_addresses,
+    ))
+    .unwrap();
+
+    let json: serde_json::Value = serde_json::from_reader(deployment_parameters_devnet).unwrap();
+
+    let delegation_manager_address = Address::parse_checksummed(
+        json.get("delegationManager").unwrap().as_str().unwrap(),
+        None,
+    )
+    .unwrap();
+
+    let avs_directory_address = Address::parse_checksummed(
+        json.get("avsDirectory").unwrap().as_str().unwrap(),
+        None,
+    )
+    .unwrap();
+
+    let strategy_manager_address = Address::parse_checksummed(
+        json.get("strategyManager").unwrap().as_str().unwrap(),
+        None,
+    )
+    .unwrap();
+
+    let rewards_coordinator_address = Address::parse_checksummed(
+        json.get("avsDirectory").unwrap().as_str().unwrap(),
+        None,
+    )
+    .unwrap();
+
     let el_chain_reader = ELChainReader::new(
         get_test_logger(),
         Address::ZERO,
@@ -43,7 +68,7 @@ async fn main() {
         delegation_manager_address,
         strategy_manager_address,
         rewards_coordinator_address,
-        el_chain_reader,
+        el_chain_reader.clone(),
         opt.http_endpoint.to_string(),
         opt.operator_private_key.to_string(),
     );
@@ -61,19 +86,67 @@ async fn main() {
         .await
         .unwrap();
 
-        let deposit_into_strategy = el_chain_writer
-        .deposit_erc20_into_strategy(Address::parse_checksummed(opt.strategy_deposit_address, None).unwrap(), U256::from(opt.strategy_deposit_amount))
+    let tokens = el_chain_reader
+        .get_strategy_and_underlying_erc20_token(
+            Address::parse_checksummed(opt.strategy_deposit_address.clone(), None).unwrap(),
+        )
+        .await
+        .unwrap();
+    let (_, underlying_token_contract, underlying_token) = tokens;
+
+    let provider = get_signer(
+        &opt.operator_private_key.to_string(),
+        &opt.http_endpoint.to_string(),
+    );
+    let contract_underlying_token = ERC20::new(underlying_token_contract, &provider);
+
+    let contract_call = contract_underlying_token.approve(
+        strategy_manager_address,
+        U256::from(opt.strategy_deposit_amount),
+    );
+    let _ = contract_call.send().await.unwrap().watch().await.unwrap();
+    let contract_strategy_manager = StrategyManager::new(strategy_manager_address, &provider);
+
+    let deposit_contract_call = contract_strategy_manager.depositIntoStrategy(
+        Address::parse_checksummed(opt.strategy_deposit_address, None).unwrap(),
+        underlying_token,
+        U256::from(opt.strategy_deposit_amount),
+    );
+
+    let tx: FixedBytes<32> = deposit_contract_call
+        .send()
+        .await
+        .unwrap()
+        .watch()
         .await
         .unwrap();
 
-    let _ = wait_transaction(&opt.http_endpoint, deposit_into_strategy).await.unwrap();
+    wait_transaction(&opt.http_endpoint, tx).await.unwrap();
 
+    let coprocessor_deployment_output_devnet = File::open(Path::new(
+        &opt.avs_registry_writer_addresses,
+    ))
+    .unwrap();
+
+    let json: serde_json::Value = serde_json::from_reader(coprocessor_deployment_output_devnet).unwrap();
+
+    let registry_coordinator = Address::parse_checksummed(
+        json.get("addresses").unwrap().get("registryCoordinator").unwrap().as_str().unwrap(),
+        None,
+    )
+    .unwrap();
+
+    let operator_state_retriever = Address::parse_checksummed(
+        json.get("addresses").unwrap().get("operatorStateRetriever").unwrap().as_str().unwrap(),
+        None,
+    )
+    .unwrap();
     let avs_registry_writer = AvsRegistryChainWriter::build_avs_registry_chain_writer(
         get_test_logger(),
         opt.http_endpoint.to_string(),
         opt.operator_private_key.to_string(),
-        get_registry_coordinator_address(opt.http_endpoint.to_owned()).await,
-        get_operator_state_retriever_address(opt.http_endpoint.to_owned()).await,
+        registry_coordinator,
+        operator_state_retriever,
     )
     .await
     .unwrap();
@@ -84,7 +157,6 @@ async fn main() {
     let seconds_since_epoch = now.duration_since(UNIX_EPOCH).unwrap().as_secs();
     let expiry = U256::from(seconds_since_epoch) + U256::from(10000);
     let quorum_numbers = Bytes::from_str("0x00").unwrap();
-    let socket = "socket";
 
     let tx_hash = avs_registry_writer
         .register_operator_in_quorum_with_avs_registry_coordinator(
@@ -92,7 +164,7 @@ async fn main() {
             salt,
             expiry,
             quorum_numbers,
-            socket.to_string(),
+            opt.socket,
         )
         .await
         .unwrap();
