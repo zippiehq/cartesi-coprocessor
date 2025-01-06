a52267621e73f72ef105ecb94c1220dc1c680d44
Carsten Munk <carsten@zippie.com>
2025-01-06 13:22:04 +0100

    feat: up to date anvil and eigenlayer contracts, from mainnet
    Signed-off-by: Carsten Munk <carsten@zippie.com>

--- env/eigenlayer/anvil/run-anvil-devnet.sh
+++ env/eigenlayer/anvil/run-anvil-devnet.sh
@@ -7,7 +7,7 @@ rm -rf /cartesi-lambada-coprocessor/contracts/broadcast
 anvil --load-state /root/.anvil/state.json --host 0.0.0.0 --block-time 12 &
 timeout 22 bash -c 'until printf "" 2>>/dev/null >>/dev/tcp/$0/$1; do sleep 1; done' 0.0.0.0:8545
 
-
+0xc5a5C42992dECbae36851359345FE25997F5C42d
 cast send \
     --rpc-url http://0.0.0.0:8545 \
     --private-key 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80 \
@@ -16,7 +16,7 @@ cast send \
 cast send \
     --rpc-url http://0.0.0.0:8545 \
     --private-key 0xc276a0e2815b89e9a3d8b64cb5d745d5b4f6b84531306c97aad82156000a7dd7 \
-    0x7a2088a1bFc9d81c55368AE168C2C02570cB814F "mint(address,uint256)" 0x02C9ca5313A6E826DC05Bbe098150b3215D5F821 10    
+    0xc5a5C42992dECbae36851359345FE25997F5C42d "mint(address,uint256)" 0x02C9ca5313A6E826DC05Bbe098150b3215D5F821 10    
 
 cast send \
     --rpc-url http://0.0.0.0:8545 \
@@ -26,7 +26,7 @@ cast send \
 cast send \
     --rpc-url http://0.0.0.0:8545 \
     --private-key 0x850affd0f354c8b3b3176ae914dcd90cdb0f2051d1c5e31c9bbf97a732b68a07 \
-    0x7a2088a1bFc9d81c55368AE168C2C02570cB814F "mint(address,uint256)" 0x71f897938C155D4569b9f8fbff8fBFC7A89069Fb 10    
+    0xc5a5C42992dECbae36851359345FE25997F5C42d "mint(address,uint256)" 0x71f897938C155D4569b9f8fbff8fBFC7A89069Fb 10    
 
 cast send \
     --rpc-url http://0.0.0.0:8545 \
@@ -36,7 +36,7 @@ cast send \
 cast send \
     --rpc-url http://0.0.0.0:8545 \
     --private-key 0xa38181ab9321e4bfdfe8dae9f99b05529483bdf0254bd5bcbcc51232f26b8c36 \
-    0x7a2088a1bFc9d81c55368AE168C2C02570cB814F "mint(address,uint256)" 0xEc1dc4D2a9459758DCe2bb13096F303a8FAF4c92 10    
+    0xc5a5C42992dECbae36851359345FE25997F5C42d "mint(address,uint256)" 0xEc1dc4D2a9459758DCe2bb13096F303a8FAF4c92 10    
 
 rm -f /root/.eigenlayer/operator_keys/foo.ecdsa.key.json
 echo "abcd" | /usr/local/bin/eigenlayer-orig keys import -i -k ecdsa foo  0xc276a0e2815b89e9a3d8b64cb5d745d5b4f6b84531306c97aad82156000a7dd7 2>&1 | tee /import.log
