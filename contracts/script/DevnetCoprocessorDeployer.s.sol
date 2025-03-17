ab1b83d6816ed68d88a36e3c4f95f8909a459456
not4x217 <6069441+not4x217@users.noreply.github.com>
2025-03-17 17:07:04 +0800

    split CoprocessorDeploymentLib between CopocessorDeployerBase and HelperLib

--- contracts/script/DevnetCoprocessorDeployer.s.sol
+++ contracts/script/DevnetCoprocessorDeployer.s.sol
@@ -10,36 +10,27 @@ import "forge-std/StdCheats.sol";
 import "@openzeppelin/contracts/proxy/transparent/ProxyAdmin.sol";
 
 import {EigenlayerDeploymentLib} from "./utils/EigenlayerDeploymentLib.sol";
-import {CoprocessorDeploymentLib} from "./utils/CoprocessorDeploymentLib.sol";
 import {CoprocessorDeployerBase} from "./utils/CoprocessorDeployerBase.sol";
 
 // forge script script/DevnetCoprocessorDeployer.s.sol:DevnetCoprocessorDeployer --rpc-url $RPC_URL  --private-key $PRIVATE_KEY --broadcast -vvvv
 
 contract DevnetCoprocessorDeployer is CoprocessorDeployerBase {
     function run() external {
-        // Eigenlayer contracts
-        vm.startBroadcast(msg.sender);
-
         el_deployment = EigenlayerDeploymentLib.readDeployment("./script/output/devnet_eigenlayer_deployment.json");
 
         config.operatorWhitelistEnabled = true;
         config.operatorWhitelist = new address[](1);
         config.operatorWhitelist[0] = 0x02C9ca5313A6E826DC05Bbe098150b3215D5F821;
         
-        deployment = CoprocessorDeploymentLib.deployContracts(
-            el_deployment,
-            config,
-            true,
-            true,
-            msg.sender
-        );
-
-        vm.stopBroadcast();
+        deployAvs(msg.sender, msg.sender);
+        verifyAvsDeployment();
+        
+        deployStrategy(msg.sender);
+        deployL1L2Bridge(msg.sender);
 
         this.setupOperators();
 
-        CoprocessorDeploymentLib
-            .writeDeployment(deployment, "./script/output/devnet_coprocessor_deployment.json");                
+        writeDeployment("./script/output/devnet_coprocessor_deployment.json");                
     }
 
     function setupOperators() external payable {
--- contracts/script/utils/CoprocessorDeployerBase.sol
+++ contracts/script/utils/CoprocessorDeployerBase.sol
@@ -3,23 +3,282 @@ pragma solidity ^0.8.27;
 
 import "forge-std/Script.sol";
 import "forge-std/console.sol";
+import {Vm} from "forge-std/Vm.sol";
+import {stdJson} from "forge-std/StdJson.sol";
 
+import {ProxyAdmin} from "@openzeppelin/contracts/proxy/transparent/ProxyAdmin.sol";
+import {TransparentUpgradeableProxy} from
+    "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
+import {Strings} from "@openzeppelin/contracts/utils/Strings.sol";
+
+import {IAVSDirectory} from "@eigenlayer/interfaces/IAVSDirectory.sol";
+import {IPermissionController} from "@eigenlayer/interfaces/IPermissionController.sol";
 import {IDelegationManager} from "@eigenlayer/interfaces/IDelegationManager.sol";
+import {IAllocationManager} from "@eigenlayer/interfaces/IAllocationManager.sol";
+import {IStrategy} from "@eigenlayer/interfaces/IStrategyManager.sol";
+import {IRewardsCoordinator} from "@eigenlayer/interfaces/IRewardsCoordinator.sol";
+import {StrategyFactory} from "@eigenlayer/strategies/StrategyFactory.sol";
 
-import "@eigenlayer/interfaces/IStrategy.sol";
-import "@eigenlayer/interfaces/IStrategyManager.sol";
-import "@eigenlayer-middleware/interfaces/ISlashingRegistryCoordinator.sol";
+import {BLSApkRegistry} from "@eigenlayer-middleware/BLSApkRegistry.sol";
+import {IndexRegistry} from "@eigenlayer-middleware/IndexRegistry.sol";
+import {InstantSlasher} from "@eigenlayer-middleware/slashers/InstantSlasher.sol";
+import {StakeRegistry} from "@eigenlayer-middleware/StakeRegistry.sol";
+import {IRegistryCoordinator} from "@eigenlayer-middleware/interfaces/IRegistryCoordinator.sol";
+import {ISocketRegistry, SocketRegistry} from "@eigenlayer-middleware/SocketRegistry.sol";
+import {
+    ISlashingRegistryCoordinator,
+    ISlashingRegistryCoordinatorTypes
+} from "@eigenlayer-middleware/interfaces/ISlashingRegistryCoordinator.sol";
+import {SlashingRegistryCoordinator} from
+    "@eigenlayer-middleware/SlashingRegistryCoordinator.sol";
+import {RegistryCoordinator} from "@eigenlayer-middleware/RegistryCoordinator.sol";
+import {
+    RegistryCoordinator,
+    IBLSApkRegistry,
+    IIndexRegistry,
+    IStakeRegistry
+} from
+"@eigenlayer-middleware/RegistryCoordinator.sol";
+import {IStakeRegistryTypes} from "@eigenlayer-middleware/interfaces/IStakeRegistry.sol";
+import {
+    PauserRegistry, IPauserRegistry
+} from "@eigenlayer/permissions/PauserRegistry.sol";
+import {OperatorStateRetriever} from "@eigenlayer-middleware/OperatorStateRetriever.sol";
 
-import "../../src/ERC20Mock.sol";
+import {EigenlayerDeploymentLib} from "./EigenlayerDeploymentLib.sol";
+import {UpgradeableProxyLib} from "./UpgradeableProxyLib.sol";
 
-import "./CoprocessorDeploymentLib.sol";
+import {ICoprocessor} from "../../src/ICoprocessor.sol";
+import {Coprocessor} from "../../src/Coprocessor.sol";
+import {CoprocessorServiceManager} from "../../eigenlayer/CoprocessorServiceManager.sol";
+import {ERC20Mock} from "../../src/ERC20Mock.sol";
+import {Mock_L2Coprocessor} from "../../src/Mock_L2Coprocessor.sol";
+import {MockL2CoprocessorCaller} from "../../src/Mock_L2CoprocessorCaller.sol";
+import {Mock_L1_Sender, IMock_L2Coprocessor} from "../../src/Mock_L1_Sender.sol";
+import {CoprocessorToL2} from "../../src/CoprocessorToL2.sol";
+
 import "./EigenlayerDeploymentLib.sol";
 
 contract CoprocessorDeployerBase is Script {
-    EigenlayerDeploymentLib.Deployment el_deployment;
-    CoprocessorDeploymentLib.DeploymentConfig config;
+    using stdJson for *;
+    using Strings for *;
+    using UpgradeableProxyLib for address;
 
-    CoprocessorDeploymentLib.Deployment deployment;
+    struct DeploymentConfig {
+        bool operatorWhitelistEnabled;
+        address[] operatorWhitelist;
+    }
+    
+    struct Deployment {
+        address proxyAdmin;
+        
+        address coprocessor;
+        address coprocessorServiceManager;
+        
+        address registryCoordinator;
+        address operatorStateRetriever;
+        address blsApkRegistry;
+        address indexRegistry;
+        address stakeRegistry;
+        address socketRegistry;
+        address pauserRegistry;
+        //address slasher;
+
+        address strategyToken;
+        address strategy;
+
+        address L2Coprocessor;
+        address L2CoprocessorCaller;
+        address L1Sender;
+        address coprocessorToL2;
+    }
+
+    EigenlayerDeploymentLib.Deployment el_deployment;
+    DeploymentConfig config;
+
+    Deployment deployment;
+
+    function deployAvs(address deployer, address admin) internal {
+        vm.startBroadcast(deployer);
+        
+        // 1. Deploy upgradeable proxy contracts that will point to the implementations
+        deployment.proxyAdmin = UpgradeableProxyLib.deployProxyAdmin();
+        
+        deployment.coprocessor = UpgradeableProxyLib.setUpEmptyProxy(deployment.proxyAdmin);
+        deployment.coprocessorServiceManager = UpgradeableProxyLib.setUpEmptyProxy(deployment.proxyAdmin);
+        
+        deployment.stakeRegistry = UpgradeableProxyLib.setUpEmptyProxy(deployment.proxyAdmin);
+        deployment.registryCoordinator = UpgradeableProxyLib.setUpEmptyProxy(deployment.proxyAdmin);
+        deployment.operatorStateRetriever = address(new OperatorStateRetriever());
+        deployment.blsApkRegistry = UpgradeableProxyLib.setUpEmptyProxy(deployment.proxyAdmin);
+        deployment.indexRegistry = UpgradeableProxyLib.setUpEmptyProxy(deployment.proxyAdmin);
+        deployment.socketRegistry = UpgradeableProxyLib.setUpEmptyProxy(deployment.proxyAdmin);
+        //deployment.slasher = UpgradeableProxyLib.setUpEmptyProxy(proxyAdmin);
+
+        // 2. Deploy the implementation contracts, using the proxy contracts as input
+        
+        // Deploy StakeRegistry
+        address stakeRegistryImpl = address(
+            new StakeRegistry(
+                ISlashingRegistryCoordinator(deployment.registryCoordinator),
+                IDelegationManager(el_deployment.delegationManager),
+                IAVSDirectory(el_deployment.avsDirectory),
+                IAllocationManager(el_deployment.allocationManager)
+            )
+        );
+
+        // Deploy BLSApkRegistry
+        address blsApkRegistryImpl =
+            address(new BLSApkRegistry(IRegistryCoordinator(deployment.registryCoordinator)));
+        
+        // Deploy IndexRegistry
+        address indexRegistryimpl =
+            address(new IndexRegistry(IRegistryCoordinator(deployment.registryCoordinator)));
+        
+        // Deploy InstantSlasher
+        /*
+        address instantSlasherImpl = address(
+            new InstantSlasher(
+                IAllocationManager(el_deployment.allocationManager),
+                ISlashingRegistryCoordinator(deployment.registryCoordinator),
+                deployment.incredibleSquaringTaskManager
+            )
+        );
+        */
+       
+        // Deploy SlashingRegistryCoordinator 
+        address registryCoordinatorImpl = address( 
+            new SlashingRegistryCoordinator(
+                IStakeRegistry(deployment.stakeRegistry),
+                IBLSApkRegistry(deployment.blsApkRegistry),
+                IIndexRegistry(deployment.indexRegistry),
+                ISocketRegistry(deployment.socketRegistry),
+                IAllocationManager(el_deployment.allocationManager),
+                IPauserRegistry(el_deployment.pauserRegistry)
+            )
+        );
+
+        // Deploy PauserRegistry
+        address[] memory pausers = new address[](2);
+        pausers[0] = admin;
+        pausers[1] = admin;
+        deployment.pauserRegistry = address(new PauserRegistry(pausers, admin));
+
+        // 3. Upgrade the proxy contracts to use the correct implementation contracts and initialize them
+        
+        // Upgrade StakeRegistry
+        UpgradeableProxyLib.upgrade(deployment.stakeRegistry, stakeRegistryImpl);
+        
+        // Upgrade BlsApkRegistry
+        UpgradeableProxyLib.upgrade(deployment.blsApkRegistry, blsApkRegistryImpl);
+        
+        // Upgrade IndexRegistry
+        UpgradeableProxyLib.upgrade(deployment.indexRegistry, indexRegistryimpl);
+        
+        // Upgrade and initialize SlashingRegistryCoordinator
+        bytes memory registryCoordinatorUpgradeCall = abi.encodeCall(
+            SlashingRegistryCoordinator.initialize,
+            (admin, admin, admin, 0, deployment.coprocessorServiceManager)
+        );
+        UpgradeableProxyLib.upgradeAndCall(
+            deployment.registryCoordinator, registryCoordinatorImpl, registryCoordinatorUpgradeCall
+        );
+
+         // Deploy Coprocessor
+        address coprocessorImpl = address(
+            new Coprocessor(ISlashingRegistryCoordinator(registryCoordinatorImpl))
+        );
+
+        // Deploy CoprocessorServiceManager
+        address coprocessorSerivceManagerImpl = address(
+            new CoprocessorServiceManager(
+                IAVSDirectory(el_deployment.avsDirectory),
+                IRewardsCoordinator(el_deployment.rewardsCoordinator),
+                IPermissionController(el_deployment.permissionController),
+                IAllocationManager(el_deployment.allocationManager),
+                IStakeRegistry(stakeRegistryImpl),
+                ISlashingRegistryCoordinator(registryCoordinatorImpl)
+            )
+        );
+
+        // Upgrade and initialize Coprocessor
+        bytes memory coprocessorUpgradeCall = abi.encodeCall(
+            Coprocessor.initialize,
+            (admin)
+        );
+        UpgradeableProxyLib.upgradeAndCall(
+            deployment.coprocessor, coprocessorImpl, coprocessorUpgradeCall
+        );
+
+        // Upgrade and initialize CoprocessorServiceManager
+        bytes memory coprocessorServiceManagerUpgradeCall = abi.encodeCall(
+            CoprocessorServiceManager.initialize,
+            (
+                ICoprocessor(coprocessorImpl), 
+                config.operatorWhitelistEnabled, 
+                config.operatorWhitelist, 
+                admin
+            )
+        );
+        UpgradeableProxyLib.upgradeAndCall(
+            deployment.coprocessorServiceManager, coprocessorSerivceManagerImpl, coprocessorServiceManagerUpgradeCall
+        );
+
+        /*
+        // Initialize and upgrade InstantSlasher
+        bytes memory slasherupgradecall = abi.encodeCall(
+            InstantSlasher.initialize, (address(deployment.incredibleSquaringTaskManager))
+        );
+        UpgradeableProxyLib.upgradeAndCall(deployment.slasher, instantSlasherImpl, slasherupgradecall);
+        */
+
+       vm.stopBroadcast();
+    }
+
+     function verifyAvsDeployment() internal view {
+        IBLSApkRegistry blsapkregistry =
+            IRegistryCoordinator(deployment.registryCoordinator).blsApkRegistry();
+        require(address(blsapkregistry) != address(0));
+        IStakeRegistry stakeregistry =
+            IRegistryCoordinator(deployment.registryCoordinator).stakeRegistry();
+        require(address(stakeregistry) != address(0));
+        IDelegationManager delegationmanager = IStakeRegistry(address(stakeregistry)).delegation();
+        require(address(delegationmanager) != address(0));
+    }
+
+    function deployStrategy(address deployer) internal {
+        vm.startBroadcast(deployer);
+        
+        deployment.strategyToken = address(new ERC20Mock());     
+        deployment.strategy = address(
+            StrategyFactory(el_deployment.strategyFactory)
+               .deployNewStrategy(ERC20Mock(deployment.strategyToken))
+        );
+
+        vm.stopBroadcast();
+    }
+
+    function deployL1L2Bridge(address deployer) internal {
+        vm.startBroadcast(deployer);
+        
+        deployment.L2Coprocessor = address(
+            new Mock_L2Coprocessor(address(0))
+        );
+        deployment.L2CoprocessorCaller = address(
+            new MockL2CoprocessorCaller(deployment.L2Coprocessor)
+        );
+
+        deployment.L1Sender = address(
+            new Mock_L1_Sender(IMock_L2Coprocessor(deployment.L2Coprocessor))
+            );
+        Mock_L2Coprocessor(deployment.L2Coprocessor).setL1Sender(deployment.L1Sender);
+
+        deployment.coprocessorToL2 = address(
+            new CoprocessorToL2(ISlashingRegistryCoordinator(deployment.registryCoordinator))
+        );
+
+        vm.stopBroadcast();
+    }
     
     function sendEther(address sender, address to, uint256 value) public payable {
         vm.startPrank(sender);
@@ -56,57 +315,38 @@ contract CoprocessorDeployerBase is Script {
         vm.stopPrank();
     }
 
+    function writeDeployment(string memory filePath) internal {
+         string memory parentObject = "parent object";
+        
+        string memory addresses = "addresses";
+        vm.serializeAddress(addresses, "proxyAdmin", address(deployment.proxyAdmin));
+        vm.serializeAddress(addresses, "coprocessor", address(deployment.coprocessor));
+        vm.serializeAddress(addresses, "coprocessorServiceManager", address(deployment.coprocessorServiceManager));
+        vm.serializeAddress(addresses, "registryCoordinator", address(deployment.registryCoordinator));
+        vm.serializeAddress(addresses, "operatorStateRetriever", address(deployment.operatorStateRetriever));
+        vm.serializeAddress(addresses, "blsApkRegistry", address(deployment.blsApkRegistry));
+        vm.serializeAddress(addresses, "indexRegistry", address(deployment.indexRegistry));
+        vm.serializeAddress(addresses, "stakeRegistry", address(deployment.stakeRegistry));
+        vm.serializeAddress(addresses, "socketRegistry", address(deployment.socketRegistry));
+        vm.serializeAddress(addresses, "pauserRegistry", address(deployment.pauserRegistry));
+        vm.serializeAddress(addresses, "strategyToken", address(deployment.strategyToken));
+        vm.serializeAddress(addresses, "strategy", address(deployment.strategy));
+        vm.serializeAddress(addresses, "L2Coprocessor", address(deployment.L2Coprocessor));
+        vm.serializeAddress(addresses, "L2CoprocessorCaller", address(deployment.L2CoprocessorCaller));
+        vm.serializeAddress(addresses, "L1Sender", address(deployment.L1Sender));
+        string memory adressesJson =
+            vm.serializeAddress(addresses, "coprocessorToL2", address(deployment.coprocessorToL2));
+        
+        string memory deploymentJson = vm.serializeString(parentObject, addresses, adressesJson);
+        vm.writeJson(deploymentJson, filePath);
+    }
+
+    // TODO: this doesn't actually advance by n blocks... maybe because broadcasting batches txs somehow..?
     function advanceChainByNBlocks(uint256 n) public {
         for (uint256 i = 0; i < n; i++) {
             // we transfer eth to ourselves to advance the block
             vm.broadcast(msg.sender);
             payable(msg.sender).transfer(1 wei);
         }
-    }
-
-    function convertBoolToString(bool input) public pure returns (string memory) {
-        if (input) {
-            return "true";
-        } else {
-            return "false";
-        }
-    }
-
-    function convertOperatorStatusToString(ISlashingRegistryCoordinator.OperatorStatus operatorStatus)
-        public
-        pure
-        returns (string memory)
-    {
-        if (operatorStatus == ISlashingRegistryCoordinatorTypes.OperatorStatus.NEVER_REGISTERED) {
-            return "NEVER_REGISTERED";
-        } else if (operatorStatus == ISlashingRegistryCoordinatorTypes.OperatorStatus.REGISTERED) {
-            return "REGISTERED";
-        } else if (operatorStatus == ISlashingRegistryCoordinatorTypes.OperatorStatus.DEREGISTERED) {
-            return "DEREGISTERED";
-        } else {
-            return "UNKNOWN";
-        }
-    }
-
-    // Forge scripts best practice: https://book.getfoundry.sh/tutorials/best-practices#scripts
-    function readInput(string memory inputFileName) internal view returns (string memory) {
-        string memory inputDir = string.concat(vm.projectRoot(), "/script/input/");
-        string memory chainDir = string.concat(vm.toString(block.chainid), "/");
-        string memory file = string.concat(inputFileName, ".json");
-        return vm.readFile(string.concat(inputDir, chainDir, file));
-    }
-
-    function readOutput(string memory outputFileName) internal view returns (string memory) {
-        string memory inputDir = string.concat(vm.projectRoot(), "/script/output/");
-        string memory chainDir = string.concat(vm.toString(block.chainid), "/");
-        string memory file = string.concat(outputFileName, ".json");
-        return vm.readFile(string.concat(inputDir, chainDir, file));
-    }
-
-    function writeOutput(string memory outputJson, string memory outputFileName) internal {
-        string memory outputDir = string.concat(vm.projectRoot(), "/script/output/");
-        string memory chainDir = string.concat(vm.toString(block.chainid), "/");
-        string memory outputFilePath = string.concat(outputDir, chainDir, outputFileName, ".json");
-        vm.writeJson(outputJson, outputFilePath);
     }
 }
