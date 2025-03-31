bcaa1eaaa417bb5377ee77da6cf35308d3de3c8b
not4x217 <6069441+not4x217@users.noreply.github.com>
2025-03-31 18:35:27 +0800

    wait for anvil to startup

--- env/eigenlayer/anvil/run-anvil-devnet.sh
+++ env/eigenlayer/anvil/run-anvil-devnet.sh
@@ -4,7 +4,10 @@ rm /cartesi-lambada-coprocessor/env/eigenlayer/anvil/devnet-operators-ready.flag
 rm -rf /cartesi-lambada-coprocessor/contracts/out
 rm -rf /cartesi-lambada-coprocessor/contracts/cache
 rm -rf /cartesi-lambada-coprocessor/contracts/broadcast
+
+# Spawn anvil
 anvil --load-state /root/.anvil/state.json --host 0.0.0.0 --block-time 12 &
+sleep 2
 
 # This is needed for anvil to accumulate fee history, othewise provider
 # from eigensdk-rs, used by setup-operator, will fail...
