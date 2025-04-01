00399e26a546e931d4f46a576a66437060983c3c
Carsten Munk <carsten@zippie.com>
2025-04-01 14:53:24 +0200

    feat: make anvil a bit leaner and no volume mounts
    Signed-off-by: Carsten Munk <carsten@zippie.com>

--- env/eigenlayer/anvil/run-anvil-devnet.sh
+++ env/eigenlayer/anvil/run-anvil-devnet.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-rm /cartesi-lambada-coprocessor/env/eigenlayer/anvil/devnet-operators-ready.flag
+rm /devnet-operators-ready.flag
 rm -rf /cartesi-lambada-coprocessor/contracts/out
 rm -rf /cartesi-lambada-coprocessor/contracts/cache
 rm -rf /cartesi-lambada-coprocessor/contracts/broadcast
@@ -14,6 +14,6 @@ sleep 2
 cast rpc anvil_mine 200 --rpc-url http://localhost:8545 > /dev/null
 sleep 15
 
-touch /cartesi-lambada-coprocessor/env/eigenlayer/anvil/devnet-operators-ready.flag
+touch /devnet-operators-ready.flag
 
 tail -f /dev/null
