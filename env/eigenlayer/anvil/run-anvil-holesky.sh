3ed2a507425adab7ea2eefbca20681948010db4c
not4x217 <6069441+not4x217@users.noreply.github.com>
2025-04-04 18:52:38 +0800

    update docker compose and anvil startup script for holesky fork

--- env/eigenlayer/anvil/run-anvil-holesky.sh
+++ env/eigenlayer/anvil/run-anvil-holesky.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-rm /cartesi-lambada-coprocessor/env/eigenlayer/anvil/holesky-operators-ready.flag
+rm /cartesi-lambada-coprocessor/env/eigenlayer/anvil/holesky-fork-operators-ready.flag
 
-rm /cartesi-lambada-coprocessor/contracts/script/output/coprocessor_deployment_output_holesky.json
+rm /cartesi-lambada-coprocessor/contracts/script/output/holesky_fork_coprocessor_deployment.json
 
 rm -rf /cartesi-lambada-coprocessor/contracts/out
 rm -rf /cartesi-lambada-coprocessor/contracts/cache
@@ -9,62 +9,19 @@ rm -rf /cartesi-lambada-coprocessor/contracts/broadcast
 
 ANVIL_FORK_URL=`cat /run/secrets/anvil_fork_url_holesky`
 anvil --hardfork cancun --fork-url $ANVIL_FORK_URL --host 0.0.0.0 --block-time 12 -vvvvv &
-timeout 22 bash -c 'until printf "" 2>>/dev/null >>/dev/tcp/$0/$1; do sleep 1; done' 0.0.0.0:8545
-
-
-cast send \
-    --rpc-url http://0.0.0.0:8545 \
-    --private-key 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80 \
-    --value 30ether 0x02C9ca5313A6E826DC05Bbe098150b3215D5F821
-
-cast send \
-    --rpc-url http://0.0.0.0:8545 \
-    --private-key 0xc276a0e2815b89e9a3d8b64cb5d745d5b4f6b84531306c97aad82156000a7dd7 \
-    --value 20ether \
-    0x94373a4919B3240D86eA41593D5eBa789FEF3848 'deposit()'
-
-cast send \
-    --rpc-url http://0.0.0.0:8545 \
-    --private-key 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80 \
-    --value 30ether 0x71f897938C155D4569b9f8fbff8fBFC7A89069Fb
-
-cast send \
-    --rpc-url http://0.0.0.0:8545 \
-    --private-key 0x850affd0f354c8b3b3176ae914dcd90cdb0f2051d1c5e31c9bbf97a732b68a07 \
-    --value 200ether \
-    0x94373a4919B3240D86eA41593D5eBa789FEF3848 'deposit()'
-
-cast send \
-    --rpc-url http://0.0.0.0:8545 \
-    --private-key 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80 \
-    --value 30ether 0xEc1dc4D2a9459758DCe2bb13096F303a8FAF4c92
-
-cast send \
-    --rpc-url http://0.0.0.0:8545 \
-    --private-key 0xa38181ab9321e4bfdfe8dae9f99b05529483bdf0254bd5bcbcc51232f26b8c36 \
-    --value 20ether \
-    0x94373a4919B3240D86eA41593D5eBa789FEF3848 'deposit()'
-
+sleep 15
 
 cd /cartesi-lambada-coprocessor/contracts
 
 forge script \
-    script/CoprocessorDeployerHolesky.s.sol:CoprocessorDeployerHolesky \
+    script/HoleskyForkCoprocessorDeployer.s.sol:HoleskyForkCoprocessorDeployer \
     --evm-version cancun \
     --rpc-url http://0.0.0.0:8545 \
-    --private-key 0xa38181ab9321e4bfdfe8dae9f99b05529483bdf0254bd5bcbcc51232f26b8c36 \
-    --broadcast -v
+    --private-key 0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d \
+    --broadcast \
+    --ffi \
+    -v
 
-rm -f /root/.eigenlayer/operator_keys/foo.ecdsa.key.json
-echo "abcd" | /usr/local/bin/eigenlayer-holesky keys import -i -k ecdsa foo  0xc276a0e2815b89e9a3d8b64cb5d745d5b4f6b84531306c97aad82156000a7dd7 2>&1 | tee /import.log
-echo "abcd" | /usr/local/bin/eigenlayer-holesky operator register /cartesi-lambada-coprocessor/env/eigenlayer/anvil/operator.yaml 2>&1 | tee /register.log
+touch /cartesi-lambada-coprocessor/env/eigenlayer/anvil/holesky-fork-operators-ready.flag
 
-STRATEGY_MANAGER=$(jq -r .strategyManager < /cartesi-lambada-coprocessor/contracts/script/input/deployment_parameters_holesky.json)
-STRATEGY_ADDRESS=$(jq -r .wETH < /cartesi-lambada-coprocessor/contracts/script/input/deployment_parameters_holesky.json )
-STRATEGY_UNDERLYING=$(cast call --rpc-url http://0.0.0.0:8545 $STRATEGY_ADDRESS "underlyingToken()(address)")
-
-cast send --rpc-url http://0.0.0.0:8545 --private-key 0xc276a0e2815b89e9a3d8b64cb5d745d5b4f6b84531306c97aad82156000a7dd7 $STRATEGY_UNDERLYING "approve(address,uint256)" $STRATEGY_MANAGER 10
-cast send --rpc-url http://0.0.0.0:8545 --private-key 0xc276a0e2815b89e9a3d8b64cb5d745d5b4f6b84531306c97aad82156000a7dd7 $STRATEGY_MANAGER "depositIntoStrategy(address,address,uint256)" $STRATEGY_ADDRESS $STRATEGY_UNDERLYING 10
-
-touch /cartesi-lambada-coprocessor/env/eigenlayer/anvil/holesky-operators-ready.flag
 tail -f /dev/null
